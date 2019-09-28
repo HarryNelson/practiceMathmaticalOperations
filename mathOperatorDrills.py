8809a95ce60af18570a875c73e17293c1b185b77
HarryNelson <55327118+HarryNelson@users.noreply.github.com>
2019-09-28 12:31:35 -0500

    Initial commit

---
+++ mathOperatorDrills.py
@@ -0,0 +1,33 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+a = 1 + 1
+print(a)
+b = a + 1
+print(b)
+c = 10 * 10
+print(c)
+d = 6 / 2
+print(d)
+e = 2 - 1
+print(e)
+f = 2 // 1
+print(f)
+g = 5 % 2
+print(g)
+h = 4 * 15
+print(h)
+i = h + 9
+print(h)
+j = 14 - 3
+print(j)
+k = 8 // 2
+print(k)
+l = 7 % 6
+print(l)
+m = 6 / 3
+print(m)
+n = 3 + 7
+print(n)
---
+++ practiceMathmaticalOperations.py
@@ -0,0 +1,11 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+m = 2
+n = 7
+m = m //n
+m = 2
+n = 7
+m = 7%2 
